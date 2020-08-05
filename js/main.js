2939865679ee45bc722e7f4721fc50cec0609571
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2020-08-06 00:48:47 +0300

    header-initial-mobile

---
+++ js/main.js
@@ -0,0 +1,5 @@
+    // Menu opener hamburger
+    $('.menu-open, .menu a').click(function () {
+        $('.menu-collapse').toggleClass('d-none').css('order', '1');
+        $('.menu').toggleClass('menu-opened');
+    });
