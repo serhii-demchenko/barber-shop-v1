972eaabbbc1c3e08eaaec190b705ca4722c0bc88
Serhii Demchenko <dem.s.vl@gmail.com>
2020-08-12 01:20:49 +0300

    fixed mobile menu(no scroll, animation)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 		openModalBtn: document.querySelector("[data-modal-open]"),
 		closeModalBtn: document.querySelector("[data-modal-close]"),
 		modal: document.querySelector("[data-modal]"),
+		modalMenu: document.querySelector("[data-modal-menu]"),
+		bodyNoScroll: document.querySelector("[data-no-scroll]"),
 	};
 
 	refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,6 +12,8 @@
 
 	function toggleModal() {
 		refs.modal.classList.toggle("overlay-is-hidden");
+		refs.modalMenu.classList.toggle("mobile-menu-animation");
+		refs.bodyNoScroll.classList.toggle("no-scroll");
 	}
 	refs.modal.addEventListener("click", ({ target }) => {
 		target.getAttribute("data-modal-link") === "" && toggleModal();
